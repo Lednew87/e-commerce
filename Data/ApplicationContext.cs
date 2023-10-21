8caaea09f5dcf3c4756e4be29fcf0293fc0b2fae
Wendel Tonzar <wendeltonzar@gmail.com>
2023-10-21 13:54:43 -0300

    Atualização e conclusão do projeto.

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -16,7 +16,10 @@ namespace Commerce.Data
             _ = optionsBuilder
                     .UseLoggerFactory(_logger)
                     .EnableSensitiveDataLogging()
-                    .UseSqlServer("Data Source=DevDianaPC\\SQLEXPRESS;Initial Catalog=commerce;Integrated Security=SSPI;Trust Server Certificate=True");
+                    .UseSqlServer("Data Source=DevDianaPC\\SQLEXPRESS;Initial Catalog=commerce;Integrated Security=SSPI;Trust Server Certificate=True",
+                    // Método para reconectar a aplicação com a base de dados automaticamente nomeando a tabela do histórico de migrations.
+                    p=>p.EnableRetryOnFailure(maxRetryCount: 2, maxRetryDelay: TimeSpan.FromSeconds(5),
+                    errorNumbersToAdd: null).MigrationsHistoryTable("ecommerce_db"));
         }
     }
 
